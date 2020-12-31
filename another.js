2dc82f38f229f387ab00a706c326ae2e24cb415d
AbhaySharma <abhay240102@gmail.com>
2020-12-30 20:39:19 -0500

    Compiled to css and js.

--- another.js
+++ another.js
@@ -1,31 +1,28 @@
-window.onload = function (_) {
-    // Range Slider
-    var mySlider = document.getElementById("mySlider");
-    var value = document.getElementById("mySliderValue");
+"use strict";
+window.onload = (_) => {
+    const mySlider = document.getElementById("mySlider");
+    const value = document.getElementById("mySliderValue");
     value.innerText = "Value: " + mySlider.value;
-    mySlider.onchange = function (_) {
+    mySlider.onchange = (_) => {
         value.innerText = "Value: " + mySlider.value;
     };
-    // Top Button
-    var topButton = document.getElementById("topButton");
-    topButton.onclick = function (_) {
+    const topButton = document.getElementById("topButton");
+    topButton.onclick = (_) => {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     };
-    // Country Datalist
-    var seaCountries = document.getElementById("seaCountries");
-    var seaCountryArray = ["Brunei", "Cambodia", "Indonesia", "Laos", "Malaysia",
+    const seaCountries = document.getElementById("seaCountries");
+    const seaCountryArray = ["Brunei", "Cambodia", "Indonesia", "Laos", "Malaysia",
         "Myanmar (Burma)", "Philippines", "Singapore", "Thailand", "Timor-Leste", "Vietnam"];
-    for (var _i = 0, seaCountryArray_1 = seaCountryArray; _i < seaCountryArray_1.length; _i++) {
-        var seaCountryElement = seaCountryArray_1[_i];
-        var options = document.createElement("option");
+    for (const seaCountryElement of seaCountryArray) {
+        const options = document.createElement("option");
         options.value = seaCountryElement;
         seaCountries.appendChild(options);
     }
-    // Cancel Button
-    var cancelButton = document.getElementById("cancelButton");
-    var seaSelector = document.getElementById("seaSelector");
-    cancelButton.onclick = function (_) {
+    const cancelButton = document.getElementById("cancelButton");
+    const seaSelector = document.getElementById("seaSelector");
+    cancelButton.onclick = (_) => {
         seaSelector.value = "";
     };
 };
+//# sourceMappingURL=another.js.map
