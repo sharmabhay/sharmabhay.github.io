af04b5adfdf69c3be8c3ac8facfc2295ebd10501
AbhaySharma <abhay240102@gmail.com>
2021-01-06 03:04:14 -0500

    updated css and task added for html

--- scripts.js
+++ scripts.js
@@ -1,28 +1,8 @@
-"use strict";
-window.onload = (_) => {
-    const mySlider = document.getElementById("mySlider");
-    const value = document.getElementById("mySliderValue");
-    value.innerText = "Value: " + mySlider.value;
-    mySlider.onchange = (_) => {
-        value.innerText = "Value: " + mySlider.value;
-    };
-    const topButton = document.getElementById("topButton");
-    topButton.onclick = (_) => {
+window.onload = function (_) {
+    // Top Button
+    var topButton = document.getElementById("topButton");
+    topButton.onclick = function (_) {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     };
-    const seaCountries = document.getElementById("seaCountries");
-    const seaCountryArray = ["Brunei", "Cambodia", "Indonesia", "Laos", "Malaysia",
-        "Myanmar (Burma)", "Philippines", "Singapore", "Thailand", "Timor-Leste", "Vietnam"];
-    for (const seaCountryElement of seaCountryArray) {
-        const options = document.createElement("option");
-        options.value = seaCountryElement;
-        seaCountries.appendChild(options);
-    }
-    const cancelButton = document.getElementById("cancelButton");
-    const seaSelector = document.getElementById("seaSelector");
-    cancelButton.onclick = (_) => {
-        seaSelector.value = "";
-    };
 };
-//# sourceMappingURL=scripts.js.map
